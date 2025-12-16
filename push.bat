9cb131588189dba9cbf5bd3eeea4b324c49c1156
TommyOx <tommyhoward510@gmail.com>
2025-12-15 21:40:37 -0700

    I changed the push.bat file to be parameterized.

--- push.bat
+++ push.bat
@@ -1,4 +1,18 @@
+@echo off
+
+:: Set the default commit message
+set "commitMessage=Save"
+
+:: Check if a command-line argument (%1) was provided
+if not "%~1"=="" (
+    :: If an argument was provided, use it as the commit message
+    set "commitMessage=%~1"
+)
+
+:: Execute the Git commands
 git add .
-git commit -m "Save"
+git commit -m "%commitMessage%"
 git push
-sleep 3
+
+:: Pause for 3 seconds
+timeout /t 3 /nobreak
